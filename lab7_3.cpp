5709ac16af01d13397b739dbfd51b58eb05f1ce9
Phurinon <folk.5834@gmail.com>
2022-12-13 17:14:32 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,26 @@
 
 using namespace std;
 
+int adiff(int x,int y){
+  int z;
+  if(x > y){
+    z = (x-y)%360;
+  }else{
+    z = (y-x)%360;
+  }
+  if(z > 180){
+    return 360-z;
+  }
+  return z;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270)<<"\n";
+  cout << adiff(210,45)<<"\n";
+  cout << adiff(0,360)<<"\n";
+  cout << adiff(10,350)<<"\n";
+  cout << adiff(95,260)<<"\n";
+  cout << adiff(90,-90)<<"\n";
+  cout << adiff(1000,280)<<"\n";
   cout << adiff(60,244);
 }
