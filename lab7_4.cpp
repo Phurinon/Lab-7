3d1f61232af4bc70dc7498374e6464919fc9969f
Phurinon <folk.5834@gmail.com>
2022-12-13 16:09:45 +0700

    lab7_4.cpp completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -6,10 +6,17 @@ using namespace std;
 int main(){
 	
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
-	int id[] = {123,196,225,329,420,451,515,666,690,787};
-	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+	int id[] = {123,196,225,329,420,451,515,666,690,787},i=0,N=10;
+	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88},x;
 
-    //Write your code here.
+    cout << "Please input GPA: ";
+	cin >> x;
+	while(i < N){
+		if(x <= gpa[i]){
+			cout << id[i] <<" "<< name[i] <<" "<< gpa[i] << "\n";
+		}
+		i++;
+	}
 		
 	return 0;
 }
